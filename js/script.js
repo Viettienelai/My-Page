c3d0e3902d2c67e74cfe58dc62be8fb66a7cf998
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-04 00:59:07 +0700

    d

--- js/script.js
+++ js/script.js
@@ -23,10 +23,10 @@ window.addEventListener('scroll', () => {
     }
 
     // Cập nhật thay đổi dựa trên progress
-    searchBar.style.width = 350 + 30 * progress;
-    searchBar.style.height = `${50 - 10 * progress}px`;
-    searchBar.style.border-radius = 20 - 10 * progress;
-    inside.style.width = 350 + 50 * progress;
+    searchBar.style.width = (350 + 30 * progress) + 'px'; 
+    searchBar.style.height = `${50 - 10 * progress}px`; 
+    searchBar.style.borderRadius = `${20 - 10 * progress}px`;
+    inside.style.width = (350 + 50 * progress) + 'px';
     inside.style.backgroundColor = `rgb(${255 - 4 * progress}, ${255 - 28 * progress}, ${255 - 64 * progress})`;
-    inside.style.border-radius = 20 - 20 * progress;
+    inside.style.borderRadius = `${20 - 20 * progress}px`; 
 });
