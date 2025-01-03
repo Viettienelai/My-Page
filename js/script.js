ec06b0a2138d1c5f307cbaf86224d55dc82f0981
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-03 14:23:43 +0700

    ư

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const SCROLL_PHASE_ONE = 240; // Giai đoạn đầu đến 240px
 
 const INITIAL_TOP = 240; // Vị trí ban đầu
 const TARGET_TOP = 250; // Mục tiêu của top ở giai đoạn đầu
-
 const INITIAL_WIDTH = 350; // Chiều rộng ban đầu của search bar
 const TARGET_WIDTH = 400; // Chiều rộng mục tiêu ở giai đoạn đầu
 const INITIAL_RADIUS = 70; // Border-radius ban đầu
@@ -30,4 +29,5 @@ window.addEventListener('scroll', () => {
         searchBarContainer.style.top = `${newTop}px`;
         searchBar.style.width = `${newWidth}px`;
         searchBar.style.borderRadius = `${newRadius}px`;
-    } else if (scrollY > SCROLL_PHASE_ONE && scrollY <= SCROLL_PHASE_TWO) {
+    }
+});
