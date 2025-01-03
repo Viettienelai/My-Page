d26938c12c98b74863a72eb778e97be199a2c862
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-03 23:21:34 +0700

    f

--- js/script.js
+++ js/script.js
@@ -8,13 +8,18 @@ window.addEventListener('scroll', () => {
     // Lấy vị trí cuộn trang
     const scrollTop = window.scrollY;
 
-    // Nếu vị trí cuộn nằm trong khoảng từ 0 đến 300px, di chuyển container lên, Nếu vị trí cuộn lớn hơn 300px, giữ container ở vị trí cố định
-    if (scrollTop <= 300) { earchBarContainer.style.top = 290 - scrollTop + 'px';} else { searchBarContainer.style.top = '-10px'; }
+    // Nếu vị trí cuộn nằm trong khoảng từ 0 đến 300px, di chuyển container lên
+    if (scrollTop <= 300) {
+        searchBarContainer.style.top = 290 - scrollTop + 'px';
+    } else {
+        // Nếu vị trí cuộn lớn hơn 300px, giữ container ở vị trí cố định
+        searchBarContainer.style.top = '-10px';
+    }
 
     // Tính toán tỷ lệ thay đổi hiệu ứng 1 (từ 0 đến 1)
     const progress1 = Math.min(scrollTop / 300, 1);
 
-    // Cập nhật border-radius, width và height của bar dựa trên progress1
+    // Cập nhật thay đổi dựa trên progress1
     const newWidth = 350 + 50 * progress1;
     searchBar.style.borderRadius = `${20 - 20 * progress1}px`;
     searchBar.style.width = `${newWidth}px`;
@@ -22,9 +27,11 @@ window.addEventListener('scroll', () => {
 
     // Tính toán tỉ lệ thay đổi hiệu ứng 2
     let progress2 = 0; // Khởi tạo giá trị mặc định
-    if (scrollTop >= 260) { progress2 = Math.min((scrollTop - 260) / 40, 1); }
+    if (scrollTop >= 260) {
+        progress2 = Math.min((scrollTop - 260) / 40, 1);
+    }
 
-    // Cập nhật màu sắc dựa trên progress2
-    searchBar.style.backgroundColor = `rgb(${255 - (4 * progress2)}, ${255 - (28 * progress2)}, ${255 - (64 * progress2)})`;
-    header.style.opacity = `${1 - progress2}`;
+    // Cập nhật thay đổi dựa trên progress2
+    searchBar.style.backgroundColor = `rgb(${255 - 4 * progress2}, ${255 - 28 * progress2}, ${255 - 64 * progress2})`;
+    header.style.opacity = 1 - progress2;
 });
