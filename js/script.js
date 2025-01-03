90bc2211910f1e29e68584153747c3c7c26cd96d
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-03 16:13:38 +0700

    f

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', function() {
     const scrollTop = window.scrollY;
     // Điều chỉnh vị trí cho phần tử di chuyển lên xuống khi cuộn
     if (scrollTop >= 240) {
-        searchBarContainer.style.transform = 'translate(-50%, 240px)';
+        searchBarContainer.style.transform = 'translate(-50%, -240px)';
     } else {
         searchBarContainer.style.transform = `translate(-50%, ${scrollTop}px)`;
     }
