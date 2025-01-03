46d37927d4b2e60a09324562bb6967ebfe59e0b9
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-03 19:10:58 +0700

    j

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const searchBar = document.getElementById('search-bar');
 const searchBarContainer = document.getElementById('search-bar-container');
 
-const containerHeight = 290;
-const searchBarWidthMax = 400;
-const searchBarBorderRadiusMax = 20;
-
 window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
-    const progress = scrollTop <= containerHeight ? scrollTop / containerHeight : 1;
 
-    const translateYValue = Math.max(0, containerHeight - scrollTop);
+    if (scrollTop <= 290) {
+        searchBarContainer.style.transform = `translateY(${290 - scrollTop}px) translateX(-50%)`;
+    } else {
+        searchBarContainer.style.transform = 'translateY(0) translateX(-50%)';
+    }
 
-    searchBarContainer.style.transform = `translateY(${translateYValue}px)`;
-    searchBar.style.borderRadius = `${searchBarBorderRadiusMax - searchBarBorderRadiusMax * progress}px`;
-    searchBar.style.width = `${350 + searchBarWidthMax * progress}px`;
+    const progress = Math.min(scrollTop / 290, 1);
+
+    searchBar.style.borderRadius = `${20 - 20 * progress}px`;
+    searchBar.style.width = `${350 + 50 * progress}px`;
 });
