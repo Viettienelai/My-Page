5588ca9388fa4b74c75a8611bacf7d654b13aae6
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-03 19:57:38 +0700

    d

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener("scroll", function() {
     // Điều kiện khi scroll từ 0 đến 290px
     if (scrollY <= 290) {
         var translateY = (scrollY / 290) * 290;  // Tính giá trị translateY từ 0 đến 290
-        searchBarContainer.style.transform = "translateY(-" + translateY + "px)";  // Di chuyển container theo scroll
+        searchBarContainer.style.transform = "translate(-50%, -" + translateY + "px)";
 
         // Tính toán width và border-radius cho search bar
         var newWidth = 350 + (scrollY / 290) * (400 - 350);  // Tăng dần width từ 350px đến 400px
