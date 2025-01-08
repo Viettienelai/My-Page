930e7779fe61cef4f31ce3d86478d3ced09ca7da
Viettienelai <vtkjbtlvkh@gmail.com>
2025-01-08 13:35:11 +0700

    c

--- js/script.js
+++ js/script.js
@@ -7,18 +7,29 @@ const icon = document.getElementById('search-icon');
 
 // Thêm trình nghe sự kiện cuộn (scroll) vào cửa sổ
 window.addEventListener('scroll', () => {
-// Lấy vị trí cuộn trang
+    // Lấy vị trí cuộn trang
     const scrollTop = window.scrollY;
 
-// Nếu vị trí cuộn nằm trong khoảng từ 0 đến 300px, di chuyển container lên
+    // Kiểm tra nếu màn hình có độ rộng nhỏ hơn 768px
+    const isMobile = window.innerWidth < 768;
+    
+    // Điều chỉnh độ rộng của BarContainer dựa trên kích thước màn hình
+    if (isMobile) {
+        BarContainer.style.width = '100%';  // 100% cho màn hình di động
+    } else {
+        // Tính toán độ rộng BarContainer với progress2 cho màn hình lớn
+        BarContainer.style.width = (50 + 50 * progress2) + '%';
+    }
+
+    // Nếu vị trí cuộn nằm trong khoảng từ 0 đến 300px, di chuyển container lên
     if (scrollTop <= 290) {
         BarContainer.style.top = 290 - scrollTop + 'px';
     } else {
-    // Nếu vị trí cuộn lớn hơn 300px, giữ container ở vị trí cố định
+        // Nếu vị trí cuộn lớn hơn 300px, giữ container ở vị trí cố định
         BarContainer.style.top = '0px';
     }
-    
-// Tính toán tỉ lệ thay đổi hiệu ứng 1
+
+    // Tính toán tỉ lệ thay đổi hiệu ứng 1
     let progress1 = Math.min(scrollTop / 230, 1);
 
     if (scrollTop <= 230) {
@@ -26,11 +37,12 @@ window.addEventListener('scroll', () => {
     } else {
         headerVideo.style.top = '230px';
     }
-// Cập nhật thay đổi dựa trên progress1
+
+    // Cập nhật thay đổi dựa trên progress1
     headerVideo.style.height = 250 - 200 * progress1 + 'px';
     headerVideo.style.width = 250 - 200 * progress1 + 'px';
 
-// Tính toán tỉ lệ thay đổi hiệu ứng 2
+    // Tính toán tỉ lệ thay đổi hiệu ứng 2
     let progress2 = 0; // Khởi tạo giá trị mặc định
     if (scrollTop >= 240) {
         progress2 = Math.min((scrollTop - 240) / 60, 1);
@@ -43,7 +55,7 @@ window.addEventListener('scroll', () => {
     let minInsideWidth = (100 * 350 / 390); 
     let remainingInsideWidth = 100 - minInsideWidth;
 
-// Cập nhật thay đổi dựa trên progress2
+    // Cập nhật thay đổi dựa trên progress2
     Bar.style.top = (0 + 10 * progress2) + 'px';
     Bar.style.width = (minBarWidth + changeBarWidth * progress2) + '%';
     Bar.style.height = (50 - 10 * progress2) + 'px';
@@ -105,10 +117,6 @@ document.addEventListener('click', (event) => {
     }
 });
 
-
-
-
-
 // Lắng nghe sự kiện submit form
 document.getElementById('search-form').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -147,5 +155,4 @@ document.querySelectorAll('.container1, .container2').forEach((container) => {
         item.style.animationDelay = `${parseFloat(items[index - 1].style.animationDelay) + 0.1}s`;
       }
     });
-  });
-  
+});
